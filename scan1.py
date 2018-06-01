e70a9545831ff64a6f408a99d4b9846f254391e8
Yash Sarvaiya <yashsarvaiya@Yashs-MacBook-Air.local>
2018-06-01 15:04:58 +0530

    commit

--- scan1.py
+++ scan1.py
@@ -23,13 +23,13 @@ image = imutils.resize(image, height = 500)
 
 # convert the image to grayscale, blur it, and find edges
 # in the image
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+gray = cv2.cvtColor(image.copy(), cv2.COLOR_BGR2GRAY)
 hsv=cv2.cvtColor(image.copy(), cv2.COLOR_BGR2HSV)
 hue, saturation, value = cv2.split(hsv)
 #clahe1 = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
 #cl2 = clahe1.apply(gray)
 #value = cv2.GaussianBlur(value, (3, 3), 0)
-
+#print(gray.shape)
 value = cv2.medianBlur(value,5)
 #value = cv2.bilateralFilter(value,5,75,75)
 th = cv2.adaptiveThreshold(value,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,\
@@ -63,7 +63,7 @@ cv2.destroyAllWindows()
 # largest ones, and initialize the screen contour
 cnts = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 cnts = cnts[0] if imutils.is_cv2() else cnts[1]
-cnts = sorted(cnts, key = cv2.contourArea, reverse = True)[:10]
+cnts = sorted(cnts, key = cv2.contourArea, reverse = True)[:5]
 
 # loop over the contours
 for c in cnts:
@@ -78,26 +78,64 @@ for c in cnts:
 		break
 
 
+def step3():
+	warped = cv2.cvtColor(image.copy(), cv2.COLOR_BGR2GRAY)
+	#print(warped)
+	#h, s, v = cv2.split(warped)
+	#arr=np.asarray(warped)
+	#dst=np.zeros(shape=(5,2))
+	#arr1=arr.flatten()
+
+	#print(np.max(arr))
+	#print(np.min(arr))
+	#for i in range(len(arr1)):
+	#	arr1[i]=((arr1[i]-np.min(arr1))/np.max(arr1))*255
+	#cv2.normalize(warped, norm, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX,dtype=cv2.CV_32F)
+	#kernel = np.array([[-1,-1,-1],[-1,9,-1],[-1,-1,-1]])
+	#frame=image.copy()
+	#smooth=cv2.GaussianBlur(frame, (3, 3), 0)
+	#cv2.addWeighted(frame,2.0,smooth, -0.9, 0,smooth)
+
+	kernel = np.array([[0,-1,0],[-1,5,-1],[0,-1,0]])
+	im=cv2.filter2D(orig,-1,kernel)
+	
+	#clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
+	#cl1 = clahe.apply(warped.copy())
+	#b=cv2.normalize(image, None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX)
+	#print(norm)
+	#out=cv2.cvtColor(cl1, cv2.COLOR_GRAY2BGR)
+
+	imginv=cv2.bitwise_not(im.copy())
+
+	T = threshold_local(warped , 25, offset = 10, method = "gaussian")
+	warped = (warped > T).astype("uint8") * 255
+	#equ = cv2.equalizeHist(warped)
+
+	
+	print("STEP 3: Apply perspective transform")
+	cv2.imshow("Original", imutils.resize(orig, height = 650))
+#	cv2.imshow("Scanned", imutils.resize(warped, height = 650))
+	cv2.imshow("Inverted", imutils.resize(imginv, height = 650))
+	cv2.imshow("Clear", imutils.resize(im, height = 650))
+#	cv2.imshow("Smooth", imutils.resize(smooth, height = 650))
+#	cv2.imshow("HistEqu", imutils.resize(cl1, height = 650))
+#	cv2.imshow("Normalized", imutils.resize(b, height = 650))
+	cv2.waitKey(0)
+	sys.exit(0)
+
+
 try:
 	screenCnt
 
 except NameError:
-	
-	warped = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-	clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
-	cl1 = clahe.apply(warped)
-	T = threshold_local(warped , 21, offset = 10, method = "gaussian")
-	warped = (warped > T).astype("uint8") * 255
-	print("STEP 3: Apply perspective transform")
-	cv2.imshow("Original", imutils.resize(orig, height = 650))
-	cv2.imshow("Scanned", imutils.resize(warped, height = 650))
-	cv2.imshow("HistEqu", imutils.resize(cl1, height = 650))
-	cv2.waitKey(0)
-	sys.exit(0)
+	step3()
+
 
 else:
-
+	if cv2.contourArea(screenCnt)<9000:
+		step3()
 	# show the contour (outline) of the piece of paper
+	print(cv2.contourArea(screenCnt))
 	print("STEP 2: Find contours of paper")
 	print(screenCnt)
 	cv2.drawContours(image, [screenCnt], -1, (0, 255, 0), 2)
@@ -111,11 +149,22 @@ else:
 
 	# convert the warped image to grayscale, then threshold it
 	# to give it the black and white paper effect
+	
+	kernel = np.array([[0,-1,0],[-1,5,-1],[0,-1,0]])
+	im=cv2.filter2D(warped,-1,kernel)
+
+
+
 	warped = cv2.cvtColor(warped, cv2.COLOR_BGR2GRAY)
 	T = threshold_local(warped, 11, offset = 10, method = "gaussian")
 	warped = (warped > T).astype("uint8") * 255
+	
+	#kernel = np.array([[0,-1,0],[-1,5,-1],[0,-1,0]])
+	#warped=cv2.filter2D(warped,-1,kernel)
 
 	print("STEP 3: Apply perspective transform")
 	cv2.imshow("Original", imutils.resize(orig, height = 650))
 	cv2.imshow("Scanned", imutils.resize(warped, height = 650))
+	cv2.imshow("Clear", imutils.resize(im, height = 650))
 	cv2.waitKey(0)
+
